ea5b572f6485192e845f532457f01e1258f2dfe5
Administrator <Administrator@smart000>
2018-02-08 12:07:03 +0900

    (차선영)Transaction이노테이션 추가
    CityService-Transaction이노테이션 추가

--- test26/src/main/java/ksmart/project/test26/service/CityService.java
+++ test26/src/main/java/ksmart/project/test26/service/CityService.java
@@ -6,8 +6,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class CityService {
 
 	@Autowired
